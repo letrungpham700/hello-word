ecc58320a71f35054b7d7b68b01d0dc925d06ca5
LeTrungPham <53925226+letrungpham700@users.noreply.github.com>
2023-12-12 08:42:13 +0700

    Push

--- Demo/lib/src/test/java/_Demo/hello.java
+++ Demo/lib/src/test/java/_Demo/hello.java
@@ -9,30 +9,4 @@ public class hello {
 
 	}
 
-
-
-	/*
-	public static String[] inputStudent() {
-		Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-		String name = scanner.nextLine();
-		System.out.print("Enter your age: ");
-        String age = scanner.nextLine();
-        System.out.print("Enter your phone: ");
-        String phone = scanner.nextLine();
-        return new String[]{name, age, phone};
-    }
-	
-
-	public static void studentInfo(String args[]) {
-        String[] studentData = inputStudent();
-        String name = studentData[0];
-        String age = studentData[1];
-        String phone = studentData[2];
-        System.out.println();
-        System.out.printf("Hello %s!\n", name);
-        System.out.printf("Age: %s\n", age);
-        System.out.printf("Phone: %s\n", phone);
-    }
-    */
 }
