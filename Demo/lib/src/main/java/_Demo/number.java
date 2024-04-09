e3bd8a26b1f7576c84376a4041305a1f9c79c985
LeTrungPham <53925226+letrungpham700@users.noreply.github.com>
2024-04-09 14:22:18 +0700

    conmoits

---
+++ Demo/lib/src/main/java/_Demo/number.java
@@ -0,0 +1,13 @@
+package _Demo;
+
+import java.util.Random;
+
+public class number {
+	public static void numberInfo(String[] args) {
+		Random random = new Random();
+
+		int randomNumber = random.nextInt(101);
+
+		System.out.println("Số ngẫu nhiên là: " + randomNumber);
+	}
+}
